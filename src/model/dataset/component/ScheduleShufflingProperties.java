d2c2fb1376e02504e63e1a1c62850655a727ab2a
syafiq <Syafiq,rezpector@gmail.com>
2016-08-08 12:55:15 +0700

    Update ScheduleShufflingProperties

--- src/model/dataset/component/ScheduleShufflingProperties.java
+++ src/model/dataset/component/ScheduleShufflingProperties.java
@@ -22,46 +22,6 @@ public class ScheduleShufflingProperties implements Cloneable
         this.classroom_current_time = classroom_current_time;
     }
 
-/*
-    @Override public ScheduleShufflingProperties clone()
-    {
-        final int[] day_set_source      = this.day_set;
-        final int[] day_set_destination = new int[this.day_set.length];
-
-        final int[][][]   classroom_set_source               = this.classrooms_set;
-        final int[][][]   classroom_set_destination          = new int[this.classrooms_set.length][][];
-        final int[][][]   lesson_set_source                  = this.lessons_set;
-        final int[][][]   lesson_set_destination             = new int[this.lessons_set.length][][];
-        final int[][][][] classroom_current_time_source      = this.classroom_current_time;
-        final int[][][][] classroom_current_time_destination = new int[this.classroom_current_time.length][][][];
-
-        System.arraycopy(day_set_source, 0, day_set_destination, 0, day_set_source.length);
-
-        for(int counter_entity = -1, entity_size = classroom_set_source.length; ++counter_entity < entity_size; )
-        {
-            classroom_set_destination[counter_entity] = new int[classroom_set_source[counter_entity].length][];
-            lesson_set_destination[counter_entity] = new int[lesson_set_source[counter_entity].length][];
-            classroom_current_time_destination[counter_entity] = new int[classroom_current_time_source[counter_entity].length][][];
-
-            for(int counter_entity_child = -1, entity_child_size = classroom_set_source[counter_entity].length; ++counter_entity_child < entity_child_size; )
-            {
-                classroom_set_destination[counter_entity][counter_entity_child] = new int[classroom_set_source[counter_entity][counter_entity_child].length];
-                lesson_set_destination[counter_entity][counter_entity_child] = new int[lesson_set_source[counter_entity][counter_entity_child].length];
-
-                System.arraycopy(classroom_set_source[counter_entity][counter_entity_child], 0, classroom_set_destination[counter_entity][counter_entity_child], 0, classroom_set_source[counter_entity][counter_entity_child].length);
-                System.arraycopy(lesson_set_source[counter_entity][counter_entity_child], 0, lesson_set_destination[counter_entity][counter_entity_child], 0, lesson_set_source[counter_entity][counter_entity_child].length);
-            }
-
-            for(int counter_classroom = -1, classroom_size = classroom_current_time_source[counter_entity].length; ++counter_classroom < classroom_size; )
-            {
-                classroom_current_time_destination[counter_entity][counter_classroom] = new int[day_set.length][2];
-            }
-        }
-
-        return new ScheduleShufflingProperties(day_set_destination, classroom_set_destination, lesson_set_destination, classroom_current_time_destination);
-    }
-*/
-
     public static ScheduleShufflingProperties newInstance(ScheduleShufflingProperties properties)
     {
         final int[] day_set_source      = properties.day_set;
@@ -113,6 +73,20 @@ public class ScheduleShufflingProperties implements Cloneable
                     {
                         day[counter_property] = 0;
                     }
+                }
+            }
+        }
+    }
+
+    public void reset_classroom_current_time(int pool_index)
+    {
+        for(int[][] classrooms : this.classroom_current_time[pool_index])
+        {
+            for(int[] day : classrooms)
+            {
+                for(int counter_property = -1, property_size = day.length; ++counter_property < property_size; )
+                {
+                    day[counter_property] = 0;
                 }
             }
         }
